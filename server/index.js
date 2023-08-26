07df5e7fdcf45e2fac9aacf733336b0022f58722
sing0017 <harmansinghsidhu262@gmail.com>
2023-08-26 12:51:55 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -102,5 +102,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(3001, '0.0.0.0', () => {
-  console.log('Server is running on port 4000');
+  console.log('Server is running on port 3001');
 });
